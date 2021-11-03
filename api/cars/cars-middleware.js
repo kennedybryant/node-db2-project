f16f7ffd28aae668ca67aea95d0da35d0df2a1ba
kennedybryant <kennedy_marisa96@yahoo.com>
2021-11-02 20:01:59 -0400

    All Tests Passing

--- api/cars/cars-middleware.js
+++ api/cars/cars-middleware.js
@@ -1,5 +1,7 @@
 const Car = require('./cars-model')
 
+const vinValidator = require('vin-validator')
+
 const checkCarId = async (req, res, next) => {
   try {
     const car = await Car.getById(req.params.id)
@@ -17,15 +19,51 @@ const checkCarId = async (req, res, next) => {
 }
 
 const checkCarPayload = (req, res, next) => {
-  // DO YOUR MAGIC
+  try {
+    if (!req.body.vin) {
+      res.status(400).json({
+        message: 'vin is missing'
+      })
+    } else if (!req.body.make) {
+      res.status(400).json({
+        message: 'make is missing'
+      })
+    } else if (!req.body.model) {
+      res.status(400).json({
+        message: 'model is missing'
+      })
+    } else if (!req.body.mileage) {
+      res.status(400).json({
+        message: 'mileage is missing'
+      })
+    } else {
+      next()
+    }
+  } catch (err) {
+    next(err)
+  }
+  
 }
 
 const checkVinNumberValid = (req, res, next) => {
-  // DO YOUR MAGIC
+  if (vinValidator.validate(req.body.vin)) {
+    next()
+  } else {
+    next({ status: 400, message: `vin ${req.body.vin} is invalid`})
+  }
 }
 
-const checkVinNumberUnique = (req, res, next) => {
-  // DO YOUR MAGIC
+const checkVinNumberUnique = async (req, res, next) => {
+  try {
+    const existing = await Car.getByVin(req.body.vin)
+    if (!existing) {
+      next()
+    } else {
+      next({ status: 400, message: `vin ${req.body.vin} already exists`})
+    }
+  } catch (err) {
+    next(err)
+  }
 }
 
 module.exports = {
--- api/cars/cars-model.js
+++ api/cars/cars-model.js
@@ -10,13 +10,19 @@ const getById = (id) => {
   return result
 }
 
-const create = (carData) => {
-  return db('cars').insert(carData)
-  .then()
+const getByVin = (vin) => {
+  const result = db('cars').where('vin', vin).first()
+  return result
+}
 
+const create = (carData) => {
+  return db('cars').insert(carData).then(([id]) => {
+    return getById(id)
+  })
 }
 module.exports = {
   getAll,
   getById,
+  getByVin,
   create
 }
